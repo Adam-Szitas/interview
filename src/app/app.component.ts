832c81858107d92ad75860708aa76ebbe1714b78
Adam-Szitas <example.eu.sk.number@gmail.com>
2023-04-13 23:56:06 +0200

    Init interview repo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,3 +8,27 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'interview';
 }
+
+
+
+// difference between observables & subjects (what's the subject you like the most?)
+
+// what are decorators?
+
+// what's the difference between any & unknown type?
+
+// Tips how to improve an app performance?
+
+// how do observables work
+
+// spread operator
+
+// DevTools basic functionality
+
+// NgRX store
+
+// RxJS lib
+
+// ESLint + Prettier
+
+// What is TS? What basic types we know? What are utility types?
